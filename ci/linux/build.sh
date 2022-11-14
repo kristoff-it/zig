6492e6039fae20a37eb15c1dab63905e661d95a3
Loris Cro <kappaloris@gmail.com>
2022-11-14 02:53:32 +0100

    ci

--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -1,27 +1,35 @@
 #!/bin/sh
 
+
+# Requires cmake ninja-build
+
 set -x
 set -e
 
-ARCH="$(uname -m)"
-DEPS_LOCAL="/deps/local"
-RELEASE_STAGING="$DRONE_WORKSPACE/_release/staging"
-OLD_ZIG="$DEPS_LOCAL/bin/zig"
-TARGET="${ARCH}-linux-musl"
+ZIGDIR="$(pwd)"
+TARGET="$ARCH-linux-musl"
 MCPU="baseline"
+CACHE_BASENAME="zig+llvm+lld+clang-$TARGET-0.10.0-dev.4560+828735ac0"
+PREFIX="$HOME/$CACHE_BASENAME"
+JOBS="-j2"
 
+rm -rf $PREFIX
+cd $HOME
+
+wget -nv "https://ziglang.org/deps/$CACHE_BASENAME.tar.xz"
+tar xf "$CACHE_BASENAME.tar.xz"
+
+ZIG="$PREFIX/bin/zig" 
+
+export CC="$ZIG cc -target $TARGET -mcpu=$MCPU"
+export CXX="$ZIG c++ -target $TARGET -mcpu=$MCPU"
 export PATH=$DEPS_LOCAL/bin:$PATH
-
-echo "building stage3-release with zig version $($OLD_ZIG version)"
-
-export CC="$OLD_ZIG cc -target $TARGET -mcpu=$MCPU"
-export CXX="$OLD_ZIG c++ -target $TARGET -mcpu=$MCPU"
 
 mkdir build-release
 cd build-release
 cmake .. \
-  -DCMAKE_INSTALL_PREFIX="$RELEASE_STAGING" \
-  -DCMAKE_PREFIX_PATH="$DEPS_LOCAL" \
+  -DCMAKE_INSTALL_PREFIX="stage3-release" \
+  -DCMAKE_PREFIX_PATH="$PREFIX" \
   -DCMAKE_BUILD_TYPE=Release \
   -DZIG_TARGET_TRIPLE="$TARGET" \
   -DZIG_TARGET_MCPU="$MCPU" \
@@ -35,18 +43,18 @@ unset CXX
 
 ninja install
 
-"$RELEASE_STAGING/bin/zig" build test docs \
+stage3-release/bin/zig build test docs \
   -fqemu \
   -fwasmtime \
   -Dstatic-llvm \
   -Dtarget=native-native-musl \
-  --search-prefix "$DEPS_LOCAL" \
+  --search-prefix "$PREFIX" \
   --zig-lib-dir "$(pwd)/../lib"
 
 # Produce the experimental std lib documentation.
-mkdir -p "$RELEASE_STAGING/doc/std"
-"$RELEASE_STAGING/bin/zig" test ../lib/std/std.zig \
-  -femit-docs=$RELEASE_STAGING/doc/std \
+mkdir -p "stage3-release/doc/std"
+stage3-release/bin/zig test ../lib/std/std.zig \
+  -femit-docs=stage3-release/doc/std \
   -fno-emit-bin \
   --zig-lib-dir "$(pwd)/../lib"
 
